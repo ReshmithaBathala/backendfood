7fe6f2d2e4e843ed5ac0429b235b6315a089022d
ReshmithaBathala <reshmithabathala26@gmail.com>
2024-10-23 15:53:06 +0530

    Updated files - removed errors f

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -9,7 +9,11 @@ set("strictQuery", true);
 
 export const dbconnect = async () => {
   try {
-    connect(process.env.MONGO_URI);
+    connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    });
     await seedUsers();
     await seedFoods();
     console.log("connect successfully---");
